cf1f755a0b077e808aad38bb9bd23fd981e34fe7
pierreEnoc <pierre_enoc@yahoo.fr>
2020-09-27 11:44:29 -0300

    :sparkles: add static records

--- src/pages/Records/index.tsx
+++ src/pages/Records/index.tsx
@@ -1,8 +1,49 @@
 import React from 'react';
+import './styles.css';
 
 const Records = () => (
-    <h1>Records</h1>
+  <div className="page-container">
+      <table className="records-table" cellPadding="0" cellSpacing="0">
+          <thead>
+              <tr>
+                <th>INSTANTE</th>
+                <th>NOME</th>
+                <th>IDADE</th>
+                <th>PLATAFORMA</th>
+                <th>GêNERO</th>
+                <th>TÌTULO DO GAME</th>
+               </tr>             
+          </thead>
+          <tbody> 
+              <tr>
+                <td>20/08/2020 13:45</td>
+                <td>Pierre franck </td>
+                <td>34</td>
+                <td>XBOX</td>
+                <td>Ação-Aventura</td>
+                <td>The last of Us</td>
+              </tr>
 
+              <tr>
+                <td>20/08/2020 13:45</td>
+                <td>Pierre franck </td>
+                <td>34</td>
+                <td>XBOX</td>
+                <td>Ação-Aventura</td>
+                <td>The last of Us</td>
+              </tr>
+
+              <tr>
+                <td>20/08/2020 13:45</td>
+                <td>Pierre franck </td>
+                <td>34</td>
+                <td>XBOX</td>
+                <td>Ação-Aventura</td>
+                <td>The last of Us</td>
+              </tr>
+          </tbody>
+      </table>
+  </div>
 );
 
 export default Records;
