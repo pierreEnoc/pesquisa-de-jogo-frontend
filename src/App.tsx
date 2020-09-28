8f4211a63906429b168c22af6a670a5a0d0c725a
pierreEnoc <pierre_enoc@yahoo.fr>
2020-09-27 22:01:55 -0300

    :sparkles: add recods dinamyc

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import Home from './pages/Home';
 import Routes from './route';
 
 function App() {
--- src/pages/Records/index.tsx
+++ src/pages/Records/index.tsx
@@ -1,49 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './styles.css';
+import { RecordsResponse } from './types';
+import {formatDate } from './helpers';
 
-const Records = () => (
-  <div className="page-container">
-      <table className="records-table" cellPadding="0" cellSpacing="0">
-          <thead>
-              <tr>
-                <th>INSTANTE</th>
-                <th>NOME</th>
-                <th>IDADE</th>
-                <th>PLATAFORMA</th>
-                <th>GêNERO</th>
-                <th>TÌTULO DO GAME</th>
-               </tr>             
-          </thead>
-          <tbody> 
-              <tr>
-                <td>20/08/2020 13:45</td>
-                <td>Pierre franck </td>
-                <td>34</td>
-                <td>XBOX</td>
-                <td>Ação-Aventura</td>
-                <td>The last of Us</td>
-              </tr>
+const BASE_URL = 'http://localhost:8080'
 
-              <tr>
-                <td>20/08/2020 13:45</td>
-                <td>Pierre franck </td>
-                <td>34</td>
-                <td>XBOX</td>
-                <td>Ação-Aventura</td>
-                <td>The last of Us</td>
-              </tr>
+const Records = () => {
 
-              <tr>
-                <td>20/08/2020 13:45</td>
-                <td>Pierre franck </td>
-                <td>34</td>
-                <td>XBOX</td>
-                <td>Ação-Aventura</td>
-                <td>The last of Us</td>
-              </tr>
-          </tbody>
-      </table>
-  </div>
-);
+    const [ recordsRespons, setRecordsResponse ] = useState<RecordsResponse>();
+    console.log(recordsRespons)
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/records?linesPerPage=12`)
+        .then(response => setRecordsResponse(response.data));
+    }, []);
+
+    return(
+        <div className="page-container">
+        <table className="records-table" cellPadding="0" cellSpacing="0">
+            <thead>
+                <tr>
+                  <th>INSTANTE</th>
+                  <th>NOME</th>
+                  <th>IDADE</th>
+                  <th>PLATAFORMA</th>
+                  <th>GêNERO</th>
+                  <th>TÌTULO DO GAME</th>
+                 </tr>             
+            </thead>
+            <tbody> 
+                {recordsRespons?.content.map(record => (
+                <tr key={record.id}>
+                     <td>{formatDate(record.moment)}</td>
+                     <td>{record.name}</td>
+                     <td>{record.age}</td>
+                     <td className="text-secondary">{record.gamePlatform}</td>
+                     <td>{record.genreName}</td>
+                     <td className="text-primary">{record.gameTitle}</td>
+                 </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
+  );
+}
 
 export default Records;
